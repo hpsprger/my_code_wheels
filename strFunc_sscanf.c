9f0f066a366960e8f95cb212a3829f10834581c2
hpsprger <hpsprger@gmail.com>
2020-04-24 01:53:18 +0800

    xxx

--- strFunc_sscanf.c
+++ strFunc_sscanf.c
@@ -10,3 +10,22 @@ int main(void)
 }
 //2006--3--18
 
+
+s   字符串。这将读取连续字符，直到遇到一个空格字符（空格字符可以是空白、换行和制表符）。
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int day, year;
+    char weekday[20], month[20], dtm[100];
+
+    strcpy( dtm, "Saturday March 25 1989" );
+    sscanf( dtm, "%s %s %d  %d", weekday, month, &day, &year );
+
+    printf("%s %d, %d = %s\n", month, day, year, weekday );
+
+    return(0);
+}
