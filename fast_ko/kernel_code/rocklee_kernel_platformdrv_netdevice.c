76607c8278a4d3b8116f8a2ecd713358958ae997
hpsprger <281872614@qq.com>
2022-02-08 21:12:31 +0800

    mdf

--- fast_ko/kernel_code/rocklee_kernel_platformdrv_netdevice.c
+++ fast_ko/kernel_code/rocklee_kernel_platformdrv_netdevice.c
@@ -178,9 +178,14 @@ static struct net_device_stats *rocklee_ndev_get_stats(struct net_device *dev)
 	return &priv->stats;
 }
 
-//ifconfig -a 把所有up 与 没有up的网卡都打印出来   sudo ifconfig rockllee0 up |  sudo ifconfig rockllee0 down 
+//ifconfig -a 把所有up 与 没有up的网卡都打印出来   
+//sudo ifconfig rockllee0 up 
+//sudo ifconfig rockllee0 down 
 //sudo insmod rocklee_kernel_platformdrv_netdevice.ko
 //sudo rmsmod rocklee_kernel_platformdrv_netdevice
+//sudo ifconfig rockllee0  192.168.4.66
+//sudo ifconfig rockllee0  hw ether fc:aa:14:e3:e2:88
+//sudo dmesg -c  
 static const struct net_device_ops g_rocklee_netdev_ops = {
 	.ndo_init = rocklee_ndev_init,
     .ndo_open = rocklee_ndev_open,
