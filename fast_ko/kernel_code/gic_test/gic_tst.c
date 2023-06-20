d73b8949ed57dcc745e7ca108c2955ea5433fba8
hpsprger <281872614@qq.com>
2023-06-20 17:16:42 +0800

    新增读GIC寄存器的代码，嵌入式汇编的方式写的

---
+++ fast_ko/kernel_code/gic_test/gic_tst.c
@@ -0,0 +1,91 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/version.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
+
+static unsigned int variable=0x12345678;
+static struct proc_dir_entry  *proc_entry;
+
+static int rockllee_proc_show(struct seq_file *seq, void *v)
+{
+    uint64_t  reg_val = 0x0;
+    __asm__ volatile (
+        "mrs %0, MPIDR_EL1"
+        :"=r"(reg_val)
+        :
+        :
+    );
+	seq_printf(seq, "MPIDR_EL1:0x%llx \n", reg_val); 
+
+	return 0;
+}
+
+static int rockllee_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, rockllee_proc_show, PDE_DATA(inode)); // 打开文件的时候，为该文件绑定对于的show函数
+}
+
+ssize_t	rockllee_proc_write(struct file *file, const char *buffer, size_t len, loff_t *pos)
+{
+	char *endptr;
+	long num = simple_strtol(buffer, &endptr, 10); // 转换十进制字符串为 long 类型
+	char *p = NULL;
+	printk("rockllee_proc_write ....len=%d pos=0x%x data=0x%x  num=%d \n", len, pos, buffer[0], num);
+
+	p = kmalloc(num, GFP_KERNEL);
+	if (p == NULL) {
+		printk("kmalloc fail.... p=0x%llx  num=0x%x!!! \n", p, num);
+	} else {
+		printk("kmalloc ok   p=0x%llx   num=0x%x!!! \n", p, num);
+	}
+
+	return len;
+}
+
+// seq_file系列函数是为了方便内核导出信息到 sysfs、debugfs、procfs 实现的
+// 以往的内核也存在各种形式的实现，但是都无法避免个别实现会产生一些漏洞，
+// 这一套函数实现可以让内核导出信息更加简单和统一稳定，维护起来更加方便。
+// 它包含了如下一些接口: 
+// int     seq_open(struct file *file, const struct seq_operations *op);
+// int     seq_release(struct inode *inode, struct file *file);
+// ssize_t seq_read(struct file *file, char __user *buf, size_t size, loff_t *ppos);
+// int     seq_write(struct seq_file *seq, const void *data, size_t len);
+// loff_t  seq_lseek(struct file *file, loff_t offset, int whence)；
+// 一般我们需要创建一个 file_operations 结构体去实现不同的 debugfs、 procfs。而以上函数就是用于快速实现这个操作结构体的
+
+//for 5.x kernel 
+static const struct proc_ops rockllee_proc_fops =
+{
+	.proc_open = rockllee_proc_open,
+	.proc_read = seq_read,    //读这个文件的buffer里面的内容 到用户空间进行显示
+	.proc_write = rockllee_proc_write,  //写用户空间的内容数据到这个文件的buffer中去 
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+
+//cat /proc/rockllee_test  ==> show variable and variable;s address
+static __init int rockllee_proc_init(void)
+{
+	// 创建 proc接口   /proc/rockllee_test 
+	proc_entry = proc_create_data("rockllee_test",0444, NULL, &rockllee_proc_fops, &variable); //&variable 私有数据的指针,如不要为NULL
+	if (proc_entry)
+		return 0;
+
+	return -ENOMEM;
+}
+
+static __exit void rockllee_proc_cleanup(void)
+{
+	remove_proc_entry("rockllee_test", NULL);
+}
+
+module_init(rockllee_proc_init);
+module_exit(rockllee_proc_cleanup);
+
+MODULE_AUTHOR("rockllee");
+MODULE_DESCRIPTION("proc exmaple");
+MODULE_LICENSE("GPL v2");
